f47ec844fa60ac8392de3daa8c08a4bae345d478
Emile <e.van.krieken@vu.nl>
2024-06-24 08:30:50 +0200

    Nerdfont, small plugins

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -6,7 +6,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -81,3 +81,5 @@ opt.autoindent = true -- copy indent from prv line
 opt.cursorline = true -- highlight line for cursor??
 
 opt.backspace = 'indent,eol,start'
+
+opt.foldmethod = 'manual'
